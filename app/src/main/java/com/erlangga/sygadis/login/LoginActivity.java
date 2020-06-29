99fe16bbdb964349743a7982c471c84413fa9bf0
Elsen Novraditya <novraditya@gmail.com>
2020-06-29 11:50:32 +0700

    Do not enter to scanner if login failed

--- app/src/main/java/com/erlangga/sygadis/login/LoginActivity.java
+++ app/src/main/java/com/erlangga/sygadis/login/LoginActivity.java
@@ -2,15 +2,19 @@ package com.erlangga.sygadis.login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.erlangga.sygadis.R;
 import com.erlangga.sygadis.admin.ScannerActivity;
 import com.erlangga.sygadis.local.LocalManager;
 import com.erlangga.sygadis.network.ServerManager;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -28,10 +32,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         LoginResponse loginResponse = new LocalManager(LoginActivity.this)
-            .getObject(SAVED_ADMIN, new LoginResponse(null, null), LoginResponse.class);
+                .getObject(SAVED_ADMIN, new LoginResponse(null, null), LoginResponse.class);
         if (loginResponse.getNip() != null) {
             startActivity(new Intent(
-                LoginActivity.this, ScannerActivity.class
+                    LoginActivity.this, ScannerActivity.class
             ));
             finish();
         }
@@ -48,38 +52,40 @@ public class LoginActivity extends AppCompatActivity {
                 String textNip = nip.getText().toString();
                 String textPassword = password.getText().toString();
                 ServerManager.INSTANCE.getServerApi().login(new LoginRequest(textNip, textPassword))
-                    .enqueue(new Callback<LoginResponse>() {
-                        @Override
-                        public void onResponse(Call<LoginResponse> call,
-                            Response<LoginResponse> response) {
-                            LoginResponse loginResponse = response.body();
-                            if (loginResponse != null && loginResponse.getNip() != null) {
-                                new LocalManager(LoginActivity.this)
-                                    .putObject(SAVED_ADMIN, loginResponse);
-                                Toast
-                                    .makeText(LoginActivity.this, "Login Berhasil",
-                                        Toast.LENGTH_SHORT)
-                                    .show();
-                                startActivity(new Intent(
-                                    LoginActivity.this, ScannerActivity.class
-                                ));
-                                finish();
-                            } else {
-                                // ketika tidak terdaftar / error dari database 404
-                                Toast
-                                    .makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT)
-                                    .show();
+                        .enqueue(new Callback<LoginResponse>() {
+                            @Override
+                            public void onResponse(Call<LoginResponse> call,
+                                                   Response<LoginResponse> response) {
+                                LoginResponse loginResponse = response.body();
+                                if (loginResponse != null && loginResponse.getNip() != null) {
+                                    new LocalManager(LoginActivity.this)
+                                            .putObject(SAVED_ADMIN, loginResponse);
+                                    Toast
+                                            .makeText(LoginActivity.this, "Login Berhasil",
+                                                    Toast.LENGTH_SHORT)
+                                            .show();
+                                    startActivity(new Intent(
+                                            LoginActivity.this, ScannerActivity.class
+                                    ));
+                                    finish();
+                                } else {
+                                    // ketika tidak terdaftar / error dari database 404
+                                    Log.e(LoginActivity.class.getSimpleName(), "Error", new Throwable());
+                                    Toast
+                                            .makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT)
+                                            .show();
+                                }
                             }
-                        }
 
-                        @Override
-                        public void onFailure(Call<LoginResponse> call, Throwable t) {
-                            // ketika tidak terdaftar / error dari database
-                            startActivity(new Intent(
-                                LoginActivity.this, ScannerActivity.class
-                            ));
-                        }
-                    });
+                            @Override
+                            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                                // ketika tidak terdaftar / error dari database
+                                Log.e(LoginActivity.class.getSimpleName(), "Error", new Throwable());
+                                Toast
+                                        .makeText(LoginActivity.this, "Login Error", Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        });
             }
         });
     }
